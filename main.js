b98bedebc21ba131b0f4abda520cef297dd95672
Cam-4587 <162454229+Cam-4587@users.noreply.github.com>
2024-08-05 10:41:56 +0000

    create block class to hold constructor,calculateHash and mineBlock functions

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+class Block {
+constructor(timestamp, data) {
+    this.index = 0;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = "0";
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+
+}
+
+calculateHash() {
+
+}
+
+mineBlock(difficulty){
+    
+}
+}
