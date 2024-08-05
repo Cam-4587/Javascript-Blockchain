2c72b6539ac71f87e2744bf740a437fd692c60b0
Cam-4587 <162454229+Cam-4587@users.noreply.github.com>
2024-08-05 11:26:31 +0000

    update dates from 2017 to 2020 when creating new blocks

--- main.js
+++ main.js
@@ -21,33 +21,46 @@ mineBlock(difficulty){
 }
 }
 
-class Blockchain{
-     constructor() {
-        this.chain = [this.createGenesis()];
-     }
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
 
+    createGenesisBlock() {
+        return new Block(0, "01/01/2020", "Genesis Block", "0");
+    }
 
-     createGenesis() {
-        return new Block(0,"01/01/2017", "Genesis block", "0")
-     }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
 
-     addBlock(newBlock){
+    addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
-     }
-     checkValid()  {
-        for(let i = 1; i < this.chain.length; i++) {
-            const currentBlock = thischain[i];
+    }
+
+    checkValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
-            if(currentBlock.hash !== currentBlock.calculateHash()) {
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
 
-            if(currentBlock.previousHash !== previousBlock.hash) {
+            if (currentBlock.previousHash !== previousBlock.hash) {
                 return false;
             }
         }
         return true;
-     }
+    }
 }
+
+
+let JsChain = new Blockchain();
+JsChain.addBlock(new Block("12/25/2020", {amount: 5}));
+JsChain.addBlock(new Block("12/26/2020", {amount: 10}));
+
+console.log(JSON.stringify(JsChain, null, 4));
+console.log("Is blockchain valid? " + JsChain.checkValid());
