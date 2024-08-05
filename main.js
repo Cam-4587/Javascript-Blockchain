18bdeb425fed21efffe3fefad2f5a5d02ab16428
Cam-4587 <162454229+Cam-4587@users.noreply.github.com>
2024-08-05 10:47:21 +0000

    Create SHA256 constant and pull information in calculateHash function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 constructor(timestamp, data) {
     this.index = 0;
@@ -10,10 +12,11 @@ constructor(timestamp, data) {
 }
 
 calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
 
 }
 
 mineBlock(difficulty){
-    
+
 }
 }
